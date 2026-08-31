3abe197066c9987d3a63800de8d8cc799539a8cf
Greer Guthrie <gguthrie@gguthrie.local>
2026-08-31 11:21:49 -0500

    Link exact mode handler dispatch data

---
+++ src/data/mvc2_mode_handler_table_004490C0.c
@@ -0,0 +1,31 @@
+typedef void (*Mvc2ModeHandler)(void);
+
+extern void func_0018DA70(void);
+extern void func_0018DB70(void);
+extern void func_0018DC10(void);
+extern void func_0018DC70(void);
+extern void func_0018DCF0(void);
+extern void func_0018DD90(void);
+extern void func_0018DDC0(void);
+extern void func_0018DE90(void);
+extern void func_0018DF40(void);
+extern void func_0018DFE0(void);
+extern void func_0018E070(void);
+extern void func_0018E0A0(void);
+extern void func_0018E160(void);
+extern void func_0018E1C0(void);
+extern void func_0018E260(void);
+extern void func_0018E2E0(void);
+extern void func_0018E340(void);
+extern void func_0018E350(void);
+extern void func_0018E400(void);
+extern void func_0018E470(void);
+extern void func_0018E500(void);
+extern void func_0018E5A0(void);
+
+const Mvc2ModeHandler gMvc2ModeHandlerTable_004490C0[4][22] = {
+    { func_0018DA70, func_0018DB70, func_0018DC10, func_0018DE90, func_0018DF40, func_0018DFE0, func_0018DFE0, func_0018E070, func_0018E0A0, func_0018E160, func_0018E260, func_0018E2E0, func_0018DCF0, func_0018DA70, func_0018E340, func_0018E350, func_0018E400, func_0018DD90, func_0018DDC0, func_0018E470, func_0018E500, func_0018DC70 },
+    { func_0018DA70, func_0018DB70, func_0018DC10, func_0018DE90, func_0018DF40, func_0018DFE0, func_0018DFE0, func_0018E070, func_0018E0A0, func_0018E160, func_0018E260, func_0018E2E0, func_0018DCF0, func_0018DA70, func_0018E340, func_0018E350, func_0018E400, func_0018DD90, func_0018DDC0, func_0018E470, func_0018E500, func_0018DC70 },
+    { func_0018DB70, func_0018DB70, func_0018DC10, func_0018DE90, func_0018DC10, func_0018DFE0, func_0018DFE0, func_0018E070, func_0018E0A0, func_0018E1C0, func_0018E260, func_0018E2E0, func_0018DCF0, func_0018DA70, func_0018E340, func_0018E350, func_0018E400, func_0018DB70, func_0018DC10, func_0018E470, func_0018E500, func_0018DC70 },
+    { func_0018E5A0, func_0018E5A0, func_0018E5A0, func_0018E5A0, func_0018E5A0, func_0018DFE0, func_0018DFE0, func_0018E5A0, func_0018E5A0, func_0018E5A0, func_0018E5A0, func_0018E5A0, func_0018DCF0, func_0018E5A0, func_0018E5A0, func_0018E5A0, func_0018E5A0, func_0018E5A0, func_0018E5A0, func_0018E470, func_0018E500, func_0018E5A0 },
+};
--- tools/compile_matching_data.sh
+++ tools/compile_matching_data.sh
@@ -15,6 +15,7 @@ mkdir -p "$BUILD/data"
     src/data/mvc2_dispatch_0043CB70.c \
     src/data/mvc2_action_dispatch_004BA540.c \
     src/data/jtbl_004ACA80.c \
+    src/data/mvc2_mode_handler_table_004490C0.c \
     config/data_symbol_addrs.txt
 
 for source in src/data/data_zero_*.c; do
@@ -39,3 +40,4 @@ compile_table src/data/mvc2_dispatch_0043CB70.c gMvc2Dispatch_0043CB70
 compile_table src/data/mvc2_action_dispatch_004BA540.c gMvc2ActionDispatch_004BA540
 compile_table src/data/jtbl_004ACA80.c jtbl_004ACA80
 compile_table src/data/identity_00464EEC.c gIdentity3x3_00464EEC
+compile_table src/data/mvc2_mode_handler_table_004490C0.c gMvc2ModeHandlerTable_004490C0
